c4dd264e28bd4480983103f7b924d01bdf7fc141
abrarzahed <abrarzahed6986@gmail.com>
2022-06-12 15:48:26 +0600

    challange 1 completed

--- section 09 Data-Structures-Operators/script.js
+++ section 09 Data-Structures-Operators/script.js
@@ -64,7 +64,128 @@ const restaurant = {
   },
 };
 
-console.log('========== || ============');
+restaurant.orderPizza('mushrooms', 'spinach', 'test');
+
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remain ing 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Podolwski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 11.33,
+    x: 3.25,
+    team2: 11.33,
+  },
+};
+
+// 1
+const [player1, player2] = game.players;
+console.log(player1, player2);
+
+//2
+const [gk1, ...fieldPlayers1] = player1;
+console.log(gk1, fieldPlayers1);
+
+const [gk2, ...fieldPlayers2] = player2;
+console.log(gk2, fieldPlayers2);
+
+//3
+const allPlayers = [...player1, ...player2];
+console.log(allPlayers);
+
+// 4
+const players1Final = [...player1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// 5
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+// 6
+const printGoals = function (...players) {
+  let output = `${players.length} goals were scored by respectively `;
+  for (let player of players) {
+    if (players.indexOf(player) === players.length - 1) {
+      console.log(players.indexOf(player), player);
+      output += player + '.';
+    } else if (players.indexOf(player) === players.length - 2) {
+      console.log(players.indexOf(player), player);
+      output += player + ' and ';
+    } else {
+      console.log(players.indexOf(player), player);
+      output += player + ', ';
+    }
+  }
+  return console.log(output);
+};
+printGoals(...game.scored);
+
+// 7
+team1 < team2 && console.log('Team 1 is more likely to win');
+team2 < team1 && console.log('Team 2 is more likely to win');
+team2 === team1 && console.log('The match is draw');
+
+// ================================================================ //
+
+/*
+restaurant.numGuests = 0;
+Nullish (null and undefined) operator
+const guestCorrect = restaurant.numGuests ?? 0;
+console.log(guestCorrect);
+*/
+
+// console.log('========== || ============');
 
 // short circuiting: logical and(&&) and logical or(||) can use any data types, the can return any data types and they csn be used for short circuiting.
 /*
@@ -80,10 +201,14 @@ console.log(false || 'Abrar');
 */
 
 // Real example
+/*
 restaurant.numGuests = 20;
 const guests = restaurant.numGuests || 100;
 
 console.log(guests);
+*/
+
+/*
 
 console.log('========== && ============');
 
@@ -96,6 +221,7 @@ console.log({} && 'Abrar');
 console.log(undefined && null);
 console.log(true && 'Abrar');
 console.log(false && 'Abrar');
+*/
 
 // Real example
 // if (restaurant.orderPizza) {
@@ -103,7 +229,7 @@ console.log(false && 'Abrar');
 // }
 
 // real example using short circuiting
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
 /*
 // The rest parameters. Rest because on left of =

05d24e25ffb8bfdae3255e2a247f35b89542c93b
abrarzahed <abrarzahed6986@gmail.com>
2022-06-16 12:26:34 +0600

    default parametsrs in es5 and es6

--- section 10 Closer look at function/script.js
+++ section 10 Closer look at function/script.js
@@ -1,1 +1,33 @@
 'use strict';
+// SECTION: default parameters in function
+const allBookings = [];
+const createBooking = function (
+  // default parameters in ES6 way
+  flightNum,
+  numPassenger = 1,
+  price = 100 * numPassenger
+) {
+  /*
+  ES5 way short circuiting to assign default value
+  numPassenger = numPassenger || 1;
+  price = price || 100;
+  */
+
+  const booking = {
+    flightNum,
+    numPassenger,
+    price,
+  };
+  allBookings.push(booking);
+  console.log(booking);
+};
+
+createBooking('LH321');
+createBooking('LH321', 2);
+createBooking('LH321', 5);
+
+// It is not possible to skip arguments when calling the function.
+createBooking('LH321', 1000); // here second argument will be counted as second parameter.
+
+// SOLUTION:
+createBooking('LH321', undefined);

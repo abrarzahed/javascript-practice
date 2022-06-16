68f1fdadbea268e2c12da7d822417e8149c9c6c9
abrarzahed <abrarzahed6986@gmail.com>
2022-06-16 13:15:25 +0600

    how aguments works with premetive and referance type

--- section 10 Closer look at function/script.js
+++ section 10 Closer look at function/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 // SECTION: default parameters in function
 const allBookings = [];
 const createBooking = function (
@@ -7,11 +8,11 @@ const createBooking = function (
   numPassenger = 1,
   price = 100 * numPassenger
 ) {
-  /*
-  ES5 way short circuiting to assign default value
-  numPassenger = numPassenger || 1;
-  price = price || 100;
-  */
+  
+//   ES5 way short circuiting to assign default value
+//   numPassenger = numPassenger || 1;
+//   price = price || 100;
+  
 
   const booking = {
     flightNum,
@@ -29,5 +30,36 @@ createBooking('LH321', 5);
 // It is not possible to skip arguments when calling the function.
 createBooking('LH321', 1000); // here second argument will be counted as second parameter.
 
+
 // SOLUTION:
 createBooking('LH321', undefined);
+*/
+
+// SECTION: HOW PASSING ARGUMENTS WORKS WITH PREEMPTIVE TYPE AND WITH REFERENCE TYPE
+const flightNum = 'LH212';
+const passenger = {
+  name: 'Abrar Zahed',
+  passport: 'ad6986798',
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'AT300';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 'ad6986798') {
+    alert('Checked In');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+// checkIn(flightNum, passenger);
+console.log(flightNum, passenger);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+
+newPassport(passenger);
+checkIn(flightNum, passenger);
+console.log(flightNum, passenger);

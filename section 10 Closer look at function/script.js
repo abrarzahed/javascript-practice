d28dcc5a1d1f10975f0950126ea2aaa169c779c0
abrarzahed <abrarzahed6986@gmail.com>
2022-06-23 14:04:56 +0600

    completed coding challenge

--- section 10 Closer look at function/script.js
+++ section 10 Closer look at function/script.js
@@ -274,7 +274,7 @@ COMMENT: the call and apply  method
 ***************************************** */
 
 // @@@@@@@@@@ The bind method  @@@@@@@@@@ //
-
+/*
 const bimanBangladesh = {
   airline: 'Biman Bangladesh',
   airCode: 'BM',
@@ -296,6 +296,7 @@ const book = function (flightNum, name) {
     passengerName: name,
   });
 };
+/*
 
 // @@@@@@@@@@ Does NOT work  @@@@@@@@@@ //
 // book(32, 'Jon Doe');
@@ -303,20 +304,129 @@ const book = function (flightNum, name) {
 // @@@@@@@@@@ Right way to do with call method  @@@@@@@@@@ //
 // INFO: The call method is actually a way to explicitly set the "this" keyword to the object you want.
 // book.call(bimanBangladesh, 303, 'Abrar Zahed');
-console.log(bimanBangladesh);
+// console.log(bimanBangladesh);
 
-book.call(banglaWings, 32, 'Jon Doe');
-console.log(banglaWings);
+// book.call(banglaWings, 32, 'Jon Doe');
+// console.log(banglaWings);
 
 // @@@@@@@@@@ The apply method  @@@@@@@@@@ //
 
 // INFO: Unlike call method the apply method takes an array as its second argument
 const flightData = [343, 'Jorge Miller'];
-book.apply(bimanBangladesh, flightData);
-console.log(bimanBangladesh);
+// book.apply(bimanBangladesh, flightData);
+// console.log(bimanBangladesh);
 
 // INFO: But there is an easy and efficient way of doing this by using call method with spread operator
-book.call(banglaWings, ...flightData);
-console.log(banglaWings);
+// book.call(banglaWings, ...flightData);
+// console.log(banglaWings);
 
 // @@@@@@@@@@ The bind method  @@@@@@@@@@ //
+// INFO: The bind does not immediately cal the function. Instead it store the function into variable
+
+/*
+const bookBW = book.bind(banglaWings);
+// bookBW(545, 'Jonas Steven');
+// console.log(banglaWings);
+
+const bookBW343 = book.bind(banglaWings, 343);
+// bookBW343('Marta');
+// console.log(banglaWings);
+
+// @@@@@@@@@@ With event listener  @@@@@@@@@@ //
+bimanBangladesh.planes = 200;
+bimanBangladesh.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+// bimanBangladesh.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', bimanBangladesh.buyPlane.bind(bimanBangladesh));
+
+// @@@@@@@@@@ Partial application  @@@@@@@@@@ //
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVat = addTax.bind(null, 0.23);
+// console.log(addVat(100));
+const addTax = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVat = addTax(0.23);
+console.log(addVat(100));
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favorite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what should the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+/*
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question} \n${this.options.join('\n')} \n(Write option number)`
+      )
+    );
+    console.log(answer);
+
+    typeof answer === 'number' &&
+      answer < this.options.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log('Result', this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] });
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+*/

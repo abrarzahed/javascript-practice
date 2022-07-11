b70e88e06f58826b05504eef2ddc1a2ef467910a
abrarzahed <abrarzahed6986@gmail.com>
2022-07-11 17:06:13 +0600

    dom treversing

--- section 12 advance dom bankist/script.js
+++ section 12 advance dom bankist/script.js
@@ -285,3 +285,37 @@ nav.addEventListener(
 /* 
   COMMENT: Dom traversing
 */
+const h1 = document.querySelector('h1');
+//=== downwards: selecting child  ===//
+
+// console.log(h1.querySelectorAll('.highlight'));
+
+// console.log(h1.childNodes);
+
+// console.log(h1.children); // only works on direct children
+
+// h1.children[0].style.color = '#fff';
+
+// h1.lastElementChild.style.color = 'orangered';
+
+//=== upwards: Selecting parent  ===//
+
+// console.log(h1.parentNode);
+
+// console.log(h1.parentElement);
+
+// closest parent
+// h1.closest('.header').style.background = `var(--gradient-secondary)`;
+// h1.closest('h1').style.background = `var(--gradient-primary)`;
+
+//=== sideways: selecting siblings  ===//
+// in javascript only direct siblings can be accessed
+
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+//=== selecting all level siblings  ===//
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(el => {
+  if (el !== h1) el.style.transform = 'scale(.5)';
+});

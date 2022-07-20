a1ae7a51461a8a2fbe445566b561e890e1755aeb
abrarzahed <abrarzahed6986@gmail.com>
2022-07-21 00:21:59 +0600

    completed another challenge

--- extra.js
+++ extra.js
@@ -651,3 +651,20 @@ function myFunction(arr, num) {
 console.log(myFunction([1, 2, 3], 6));
 console.log(myFunction(["a", "b"], 2));
 */
+
+/*
+// Write a function that takes two objects as arguments
+// Unfortunately, the property 'b' in the second object has the wrong key
+// It should be named 'd' instead
+// Merge both objects and correct the wrong property name
+// Return the resulting object
+// It should have the properties 'a', 'b', 'c', 'd', and 'e'
+function myFunction(x, y) {
+  // y["d"] = y["b"];
+  // delete y["b"];
+  // return { ...x, ...y };
+  const { b, ...rest } = y;
+  return { ...x, ...rest, d: b };
+}
+console.log(myFunction({ a: 1, b: 2 }, { c: 3, b: 4, e: 5 }));
+*/

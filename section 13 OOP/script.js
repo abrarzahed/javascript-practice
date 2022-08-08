6dcd224f6d72cb203e2c4550a1908f505725b8f3
abrarzahed <abrarzahed6986@gmail.com>
2022-08-08 23:29:09 +0600

    completed oop

--- section 13 OOP/script.js
+++ section 13 OOP/script.js
@@ -486,33 +486,66 @@ console.log(jay);
 /* 
   COMMENT: Another example of class
 */
-
 class Account {
+  //=== 1) Public fields (instances)  ===//
+  locale = navigator.language;
+
+  //=== 2) Private Fields   ===//
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
-    this.locale = navigator.language;
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${this.owner}`);
   }
 
-  //=== methods: interface: API  ===//
+  //=== 3) Public Methods   ===//
+  //=== interface: API  ===//
+  getMovements() {
+    return this.#movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this.#movements.push(val);
+    return this;
   }
   withdraw(val) {
-    this.movements.push(-val);
+    this.deposit(-val);
+    return this;
+  }
+
+  requestLoan(val) {
+    if (this.#approveLoan(val)) {
+      this.deposit(val);
+      console.log(`${val} loan was approved`);
+    }
+    return this;
+  }
+
+  //=== 4) Private Methods   ===//
+  #approveLoan(val) {
+    console.log('Log from approveLoan function');
+    return true;
   }
 }
 
 const acc1 = new Account('Abrar', 'EUR', 1111);
 
-// acc1.movements.push(250);
-// acc1.movements.push(-100);
-
 acc1.deposit(200);
 acc1.withdraw(100);
 
+acc1.requestLoan(1000);
+
+// console.log(acc1.#pin);
+console.log(acc1.getMovements());
+
+// console.log(acc1.#movements);
+
+//=== Chaining   ===//
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
 console.log(acc1);

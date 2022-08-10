67f3f064247a5dadd5dfec66883d2a48ac0b7dfb
abrarzahed <abrarzahed6986@gmail.com>
2022-08-10 14:48:50 +0600

    take project arcitructure

--- section 14 mapty app/script.js
+++ section 14 mapty app/script.js
@@ -3,6 +3,9 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+/****************************************** 
+COMMENT: Dom Selection   
+******************************************/
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -11,29 +14,68 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
+/****************************************** 
+COMMENT: geolocation and map render   
+******************************************/
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
+      //=== user coordinates  ===//
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
       const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 13);
+      //=== setting coordinates to map  ===//
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      //=== click event on map and handle popup  ===//
+      map.on('click', function (event) {
+        mapEvent = event;
+        //=== render the activity form  ===//
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
     },
     function () {
       alert('Could not get your position');
     }
   );
 }
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  //=== clear input fields  ===//
+  // prettier-ignore
+  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+  //=== display marker  ===//
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});

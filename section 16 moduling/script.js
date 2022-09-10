37bdc09e6e7845402509008d24c517f6fd7945b6
abrarzahed <abrarzahed6986@gmail.com>
2022-09-10 14:31:41 +0600

    added lodash

--- section 16 moduling/script.js
+++ section 16 moduling/script.js
@@ -1,13 +1,16 @@
 /* 
   COMMENT: Importing module
 */
+
 //=== Named import  ===//
+
 /*
 import { addToCart, cart, totalPrice, totalQuantity } from './shoppingCard.js';
 console.log('importing Module');
 addToCart('bread', 10);
 console.log(totalPrice, totalQuantity);
 */
+
 /*
 import * as ShoppingCart from './shoppingCard.js';
 console.log(ShoppingCart.totalPrice);
@@ -15,5 +18,60 @@ ShoppingCart.addToCart('bread', 5);
 */
 
 //=== Default import  ===//
+
+/*
 import add from './shoppingCard.js';
 add('pizza', 2);
+*/
+
+/* 
+  COMMENT: Module pattern: how module system works behind the seen
+*/
+
+/*
+const ShoppingCard2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart. Shipping cost is ${shippingCost}`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier.`);
+  };
+
+  return { cart, totalPrice, totalQuantity, addToCart };
+})();
+
+ShoppingCard2.addToCart('Alu', 20);
+
+console.log(ShoppingCard2.shippingCost);
+console.log(ShoppingCard2);
+*/
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 10 },
+    { product: 'pizza', quantity: 6 },
+  ],
+  user: { loggedIn: true },
+};
+
+/*
+const cloneState = JSON.parse(JSON.stringify(state));
+// const cloneState = Object.assign({}, state);
+console.log(cloneState);
+*/
+
+//=== with lodash  ===//
+const deepCloneState = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(state);
+console.log(deepCloneState);
